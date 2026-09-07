a48d430a38f861449317eaa44b50640211d28c5d
chethas66 <chethasgowda66@gmail.com>
2026-09-07 11:05:59 +0530

    Add Gitstatus, Learningcmd, studentdata and project settings

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/studentdata.java
@@ -0,0 +1,12 @@
+public class studentdata  {
+    public static void main(String[] args) {
+
+        // Display the student's profile.
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+    }
+}
